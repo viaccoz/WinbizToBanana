410292bac51666a8f766038a93855a7a487b5b8a
Thierry Viaccoz <42694303+viaccoz@users.noreply.github.com>
2023-12-03 17:35:52 +0100

    Update ch.viaccoz.winbiztobanana.import.winbiz.transactions.js

--- ch.viaccoz.winbiztobanana.import.winbiz.transactions.js
+++ ch.viaccoz.winbiztobanana.import.winbiz.transactions.js
@@ -12,46 +12,59 @@
 // @inputfilefilter = Comma separated files (*.csv);;Winbiz database files (*.dbf);;All files (*.*)
 // @inputfilefilter.fr = Fichiers délimités par des virgules (*.csv);;Fichiers base de données Winbiz (*.dbf);;Tous les fichiers (*.*)
 
+const fields = new Map([
+		['MULTIPLE'      ,{id:  0, length:   2}],
+		['ECR_NUMERO'    ,{id:  1, length:  10}],
+		['NUMERO'        ,{id:  2, length:  10}],
+		['ECR_NOLINE'    ,{id:  3, length:   4}],
+		['DATE'          ,{id:  4, length:   8}],
+		['PIECE'         ,{id:  5, length:  20}],
+		['LIBELLE'       ,{id:  6, length: 100}],
+		['CPT_DEBIT'     ,{id:  7, length:  12}],
+		['CPT_CREDIT'    ,{id:  8, length:  12}],
+		['MONTANT'       ,{id:  9, length:  15}],
+		['JOURNAL'       ,{id: 10, length:   1}],
+		['ECR_MONNAI'    ,{id: 11, length:  10}],
+		['ECR_COURS'     ,{id: 12, length:  16}],
+		['ECR_MONMNT'    ,{id: 13, length:  15}],
+		['ECR_MONQTE'    ,{id: 14, length:   6}],
+		['MISCELLANEOUS' ,{id: 15, length: 174}],
+]);
+
+function getValue(array, fieldName) {
+	return array[fields.get(fieldName).id].trim();
+}
+
 function exec(inputText) {
 	let dateFormat = '';
 	let inputArray = '';
 	const outputArray = [];
 	const multiple = 'multiple'.toLowerCase();
 
-	if (inputText.startsWith(String.fromCharCode(3))) {
+	const fieldLengths = [];
+	for (const field of fields.values()) {
+		fieldLengths.push(field.length);
+	}
+
+	if (inputText.startsWith(String.fromCharCode(3) + String.fromCharCode(23))) {
+		// Winbiz database files (*.dbf)
 		dateFormat = 'yyyymmdd';
 		const inputTextData = inputText.substring(inputText.indexOf('\r') + 1);
 		const inputTextDataByLine = inputTextData.replace(/.{415}/g, '$&\n');
-		inputArray = Banana.Converter.flvToArray(inputTextDataByLine, [
-				2,   // MULTIPLE
-				10,  // ECR_NUMERO
-				10,  // NUMÉRO     -> Doc
-				4,   // ECR_NOLINE
-				8,   // DATE       -> Date
-				20,  // PIÈCE
-				100, // LIBELLÉ    -> Description
-				12,  // CPT_DÉBIT  -> AccountDebit
-				12,  // CPT_CRÉDIT -> AccountCredit
-				15,  // MONTANT    -> Amount
-				1,   // JOURNAL
-				10,  // ECR_MONNAI
-				16,  // ECR_COURS
-				15,  // ECR_MONMNT
-				6,   // ECR_MONQTE
-				174
-			]);
+		inputArray = Banana.Converter.flvToArray(inputTextDataByLine, fieldLengths);
 	} else {
+		// Comma separated files (*.csv)
 		dateFormat = 'dd.mm.yyyy';
 		inputArray = Banana.Converter.csvToArray(inputText, ';', '"');
 	}
 
 	for (const inputRow of inputArray) {
-		const date = Banana.Converter.toInternalDateFormat(inputRow[4].trim(), dateFormat);
-		const doc = inputRow[2].trim();
-		const description = inputRow[6].trim();
-		const accountDebit = inputRow[7].trim();
-		const accountCredit = inputRow[8].trim();
-		const amount = inputRow[9].trim();
+		const date = Banana.Converter.toInternalDateFormat(getValue(inputRow, 'DATE'), dateFormat);
+		const doc =           getValue(inputRow, 'NUMERO');
+		const description =   getValue(inputRow, 'LIBELLE');
+		const accountDebit =  getValue(inputRow, 'CPT_DEBIT');
+		const accountCredit = getValue(inputRow, 'CPT_CREDIT');
+		const amount =        getValue(inputRow, 'MONTANT');
 
 		// Delete grouped transactions
 		if (accountDebit.toLowerCase() === multiple || accountCredit.toLowerCase() === multiple) {
