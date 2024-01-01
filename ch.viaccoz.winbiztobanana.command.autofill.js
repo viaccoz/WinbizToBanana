edee77d7ee67880d6ac664d9a6e5e5c8fcc7976d
Thierry Viaccoz <42694303+viaccoz@users.noreply.github.com>
2024-01-01 19:04:24 +0100

    Update ch.viaccoz.winbiztobanana.command.autofill.js

--- ch.viaccoz.winbiztobanana.command.autofill.js
+++ ch.viaccoz.winbiztobanana.command.autofill.js
@@ -1,6 +1,6 @@
 // @id = ch.viaccoz.winbiztobanana.command.autofill
 // @api = 1.0
-// @pubdate = 2023-12-31
+// @pubdate = 2024-01-01
 // @publisher = Thierry Viaccoz
 // @description = Autofill from the description
 // @description.fr = Remplissage automatique depuis la description
@@ -9,11 +9,9 @@
 // @task = app.command
 // @timeout = 5000
 
-const MAXIMUM_NUMBER_OF_ROWS = 20;
-
-function removeEnd(input) {
-	return input.substr(0, input.lastIndexOf(' ')).trim();
-}
+const MAXIMUM_NUMBER_OF_ROWS_SELECTABLE = 20;
+const CHARACTERS_CLASS_FOR_REPLACEMENT = 'A-Za-z0-9\\u00C0-\\u024F\\u1E00-\\u1EFF';
+const REGULAR_EXPRESSION_FOR_REPLACEMENT = new RegExp('[^' + CHARACTERS_CLASS_FOR_REPLACEMENT + ']*[' + CHARACTERS_CLASS_FOR_REPLACEMENT + ']+[^' + CHARACTERS_CLASS_FOR_REPLACEMENT + ']*$');
 
 function exec() {
 	// Check that current row is in transactions table
@@ -26,7 +24,7 @@ function exec() {
 	// Prepare start and stop
 	let start = Banana.document.cursor.selectionTop;
 	let stop = Banana.document.cursor.selectionBottom;
-	if (stop - start + 1 > MAXIMUM_NUMBER_OF_ROWS) {
+	if (stop - start + 1 > MAXIMUM_NUMBER_OF_ROWS_SELECTABLE) {
 		start = Banana.document.cursor.rowNr;
 		stop = Banana.document.cursor.rowNr;
 	}
@@ -34,30 +32,28 @@ function exec() {
 	// Iterate over all selected rows
 	let rowChange = '';
 	for (let i = start; i <= stop; i++) {
-		let currentRowDescription = transactionsTable.value(i, 'Description');
+		let description = transactionsTable.value(i, 'Description');
+		const descriptionSubstrings = [];
+		while (description.length > 0) {
+			descriptionSubstrings.push(description);
+			description = description.replace(REGULAR_EXPRESSION_FOR_REPLACEMENT, '');
+		}
 		const similarRows = transactionsTable.findRows(function(rowObj, rowNr, table) {
-			let rowObjDescription = rowObj.value('Description');
-			do {
-				if (rowObjDescription.startsWith(currentRowDescription) && rowNr !== i) {
+			const rowObjDescription = rowObj.value('Description');
+			for (const descriptionSubstring of descriptionSubstrings) {
+				if (rowObjDescription.startsWith(descriptionSubstring) && rowNr !== i) {
 					return true;
 				}
-				rowObjDescription = removeEnd(rowObjDescription);
-				currentRowDescription = removeEnd(currentRowDescription);
-			} while (rowObjDescription.length > 0 && currentRowDescription.length > 0);
+			}
 			return false;
 		});
 
 		if (similarRows.length > 0) {
-			let longestSimilarRowLength = -1;
-			let longestSimilarRowIndex = -1;
-			for (let j = 0; j < similarRows.length; j++) {
-				const currentSimilarRowLength = similarRows[j].value('Description').length;
-				if (currentSimilarRowLength > longestSimilarRowLength) {
-					longestSimilarRowLength = currentSimilarRowLength;
-					longestSimilarRowIndex = j;
-				}
-			}
-			const similarRow = similarRows[longestSimilarRowIndex];
+			// Choose similar row with longest matching length
+			const similarRow = similarRows.reduce(function (a, b) {
+				return a.value('Description').length > b.value('Description').length ? a : b;
+			});
+
 			rowChange += `{
 				"fields": {
 					"AccountDebit": "` + similarRow.value('AccountDebit') + `",
